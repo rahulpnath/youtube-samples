d43c6d11a1ca90ccd2cb0f57c02e33d54bfa9e9d
Rahul Nath <rahulpnath@gmail.com>
2023-11-29 05:21:03 +1000

    Adding Lambda Metrics using Powertools

--- lambda-powertools/lambda-powertools-metrics/src/lambda-powertools-metrics/Functions.cs
+++ lambda-powertools/lambda-powertools-metrics/src/lambda-powertools-metrics/Functions.cs
@@ -2,6 +2,7 @@ using Amazon.DynamoDBv2.DataModel;
 using Amazon.Lambda.Annotations;
 using Amazon.Lambda.Annotations.APIGateway;
 using Amazon.Lambda.Core;
+using AWS.Lambda.Powertools.Metrics;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
@@ -16,13 +17,28 @@ namespace lambda_powertools_metrics
             _dynamoDBContext = dynamodbContext;
         }
 
+        private Dictionary<string, string> _defaultDimensions = new Dictionary<string, string>()
+        {
+            {"Environment", "Prod" }
+        };
+
         [LambdaFunction(Role = "@WeatherApiLambdaExecutionRole")]
         [HttpApi(LambdaHttpMethod.Get, "/{cityName}")]
+        [Metrics(Namespace = "WeatherStats", Service = "WeatherService")]
         public async Task<List<WeatherForecast>> GetWeather(string cityName, ILambdaContext context)
         {
+            Metrics.SetDefaultDimensions(_defaultDimensions);
+            Metrics.AddDimension("CityName", cityName);
+            Metrics.AddMetric("GetWeather", 1, MetricUnit.Count, MetricResolution.High);
             var results = await _dynamoDBContext
                 .QueryAsync<WeatherForecast>(cityName)
                 .GetRemainingAsync();
+
+            if (results.Count > 10)
+            {
+                Metrics.AddDimension("High", cityName);
+                Metrics.AddMetadata("ResultCount", results.Count);
+            }
 
             return results;
         }
