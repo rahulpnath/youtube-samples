0facf886f91cc2f7613a1161d499c561eb33c3b3
Rahul Nath <rahulpnath@gmail.com>
2024-02-09 05:10:56 +1000

    Dispatch Modes

---
+++ rabbit-mq/03 dispatch-modes/Receive/Receive.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory()
+{
+    Uri = new Uri("YOUR RABBIT INSTANCE URI"),
+    Port = 5671,
+    UserName = "<USERNAME FROM CONFIGURATION FILE>",
+    Password = "<PASSWORD FROM CONFIGURATION FILE>"
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare("hello", false, false, false, null);
+channel.BasicQos(prefetchSize:0, prefetchCount:1, global:false);
+Console.WriteLine("[] Waiting For messages");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"[x] Received message {message}");
+
+    if (message.Contains("exception"))
+    {
+        Console.WriteLine("Error in processing");
+        channel.BasicReject(ea.DeliveryTag, false);
+        throw new Exception("Error in processing");
+    }
+
+    if (int.TryParse(message, out var delayTime))
+        Thread.Sleep(delayTime * 1000);
+    
+    // Additional processing for this message
+    Console.WriteLine($"Processed message {message}");
+    channel.BasicAck(deliveryTag: ea.DeliveryTag, false);
+};
+
+channel.BasicConsume("hello", autoAck: false, consumer);
+
+Console.WriteLine("Press enter to exit");
+Console.ReadLine();
---
+++ rabbit-mq/03 dispatch-modes/Send/Send.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using RabbitMQ.Client;
+
+Console.WriteLine("Hello, World!");
+
+var factory = new ConnectionFactory()
+{
+    Uri = new Uri("YOUR RABBIT INSTANCE URI"),
+    Port = 5671,
+    UserName = "<USERNAME FROM CONFIGURATION FILE>",
+    Password = "<PASSWORD FROM CONFIGURATION FILE>"
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare("hello", false, false, false, null);
+
+string? message = null;
+
+do
+{
+    Console.WriteLine("Please enter your message");
+    message = Console.ReadLine();
+    if (!string.IsNullOrEmpty(message))
+        SendMessage(channel, message);
+} while (!string.IsNullOrEmpty(message));
+
+void SendMessage(IModel channel, string message)
+{
+    var body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(string.Empty, "hello", null, body);
+    Console.WriteLine($"[x] Send {message}");
+}
