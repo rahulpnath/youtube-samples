a7a86858ac2648915816c80ea460de740c19c743
Rahul Nath <rahulpnath@gmail.com>
2024-03-14 05:15:19 +1000

    Cancellation Token

---
+++ cancellation-token-example/cancellation-token-api/Program.cs
@@ -0,0 +1,73 @@
+using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/long-running-request", async (CancellationToken cancellationToken) =>
+    {
+        var randomId = Guid.NewGuid();
+        var results = new List<string>();
+
+        for (int i = 0; i < 100; i++)
+        {
+            if (cancellationToken.IsCancellationRequested)
+                return Results.StatusCode(499);
+            
+            await Task.Delay(1000);
+            var result = $"{randomId} - Result {i}";
+            Console.WriteLine(result);
+            results.Add(result);
+        }
+
+        return Results.Ok(results);
+    })
+    .WithName("GetAllData")
+    .WithOpenApi();
+
+#region Large Upload
+
+app.MapPost("/upload-large-file", async ([FromForm] FileUploadRequest request, CancellationToken cancellationToken) =>
+    {
+        try
+        {
+            var s3Client = new AmazonS3Client();
+            await s3Client.PutObjectAsync(new PutObjectRequest()
+            {
+                BucketName = "user-service-large-messages",
+                Key = $"{Guid.NewGuid()} - {request.File.FileName}",
+                InputStream = request.File.OpenReadStream()
+            }, cancellationToken);
+
+            return Results.NoContent();
+        }
+        catch (OperationCanceledException e)
+        {
+            return Results.StatusCode(499);
+        }
+    })
+    .WithName("UploadLargeFile")
+    .DisableAntiforgery()
+    .WithOpenApi();
+
+#endregion
+
+app.Run();
+
+record FileUploadRequest(IFormFile File) { }
---
+++ cancellation-token-example/cancellation-token-example/Program.cs
@@ -0,0 +1,29 @@
+﻿var source = new CancellationTokenSource();
+Console.WriteLine("Press any key to cancel the operation...");
+var longRunningTask = LongRunningOperationAsync(source.Token);
+
+Console.ReadKey();
+Console.WriteLine("Key Pressed");
+source.Cancel();
+
+await longRunningTask;
+
+async Task LongRunningOperationAsync(CancellationToken cancellationToken)
+{
+    try
+    {
+        var i = 0;
+        while (i++ < 10)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            // Simulate some work
+            Console.WriteLine($"Working {i}");
+            await Task.Delay(2000, cancellationToken);
+            Console.WriteLine($"Completed {i}");
+        }
+    }
+    catch (OperationCanceledException )
+    {
+        Console.WriteLine("User cancelled the operation");
+    }
+}
