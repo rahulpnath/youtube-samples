124d675c295176cb33ef3a5bb19e9215f0005841
Rahul Nath <rahulpnath@gmail.com>
2024-06-21 05:29:54 +1000

    primary constructors

---
+++ primary-constructors/primary-constructors/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public class Person(string name, int age)
+{
+    public string Name { get; set; } = name;
+    public int Age { get; set; } = age;
+
+    public Person() : this("Rahul", 30)
+    {
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {name}, Age: {age}");
+    }
+}
