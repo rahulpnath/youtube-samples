39b466fc0bd68c86f1e817a8f077f009e97f0feb
Rahul Nath <rahulpnath@gmail.com>
2024-07-26 05:39:43 +1000

    ECS and Design Principles

---
+++ design-principles/design-principles/DRY.cs
@@ -0,0 +1,48 @@
+﻿
+
+
+
+
+
+public class UserManager
+{
+    public void ProcessUser(User user)
+    {
+        if (IsUserUnderage(user.Age))
+            return;
+        
+        Console.WriteLine("Processing User");
+        // Process user...
+    }
+
+    public void RegisterUser(User user)
+    {
+        if (IsUserUnderage(user.Age))
+            return;
+        
+        Console.WriteLine("Registering User");
+        // Register user...
+    }
+    
+    private bool IsUserUnderage(User user)
+    {
+        if (user.Age < 21)
+        {
+            Console.WriteLine("User is underage");
+            LogAction("Underage user detected");
+            return true;
+        }
+        return false;
+    }
+
+    #region Other Methods
+
+    public class User
+    {
+        public int Age { get; set; }
+    }
+
+    public void LogAction(string message){}
+
+    #endregion
+}
---
+++ design-principles/design-principles/Tell Dont Ask.cs
@@ -0,0 +1,31 @@
+﻿public class OrderManager
+{
+    public void ProcessOrder(Order order)
+    {
+       order.ProcessLargeOrderDiscount();
+    }
+}
+
+public class Order
+{
+    public decimal TotalAmount { get; private set; }
+    public bool IsDiscountApplied { get; private set; }
+
+    public void ApplyDiscount(decimal discountPercentage)
+    {
+        if (!IsDiscountApplied)
+        {
+            TotalAmount -= TotalAmount * 
+                           (discountPercentage / 100);
+            IsDiscountApplied = true;
+        }
+    }
+    
+    public void ProcessLargeOrderDiscount()
+    {
+        if (TotalAmount > 1000)
+        {
+            ApplyDiscount(10);
+        }
+    }
+}
