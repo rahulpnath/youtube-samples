d9ab2eca90ccb8c96ffd9c3aae9c1115fa7469f7
Rahul Nath <rahulpnath@gmail.com>
2024-08-17 05:23:59 +1000

    ASP NET Application URLs

---
+++ asp-urls/asp-urls/Program.cs
@@ -0,0 +1,56 @@
+using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// builder.WebHost.UseUrls("https://localhost:7126", "http://localhost:5001");
+
+// builder.WebHost.ConfigureKestrel(options => options.Listen(
+//     IPAddress.Any, 7234,
+//     listenOptions => listenOptions.UseHttps()));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.Urls.Add("http://localhost:5003");
+// app.Urls.Add("https://localhost:7146");
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+app.Run(
+    // "https://localhost:7654"
+);
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
+}
