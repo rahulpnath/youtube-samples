9b2b152e3fa03504be606bddbf68122df804a5ad
Rahul Nath <rahulpnath@gmail.com>
2025-09-23 05:34:27 +1000

    SQS FIFO

---
+++ sqs-fifo/sqs-fifo/Program.cs
@@ -0,0 +1,74 @@
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using System.Text.Json;
+using Microsoft.Extensions.Options;
+using sqs_fifo;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+// Configure SQS settings with options pattern
+builder.Services.Configure<SqsSettings>(builder.Configuration.GetSection("SqsSettings"));
+
+builder.Services.AddAWSService<IAmazonSQS>();
+builder.Services.AddHostedService<WeatherForecastProcessor>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", (string city) =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    Guid.NewGuid(), 
+                    city,
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast");
+
+app.MapPost("/weatherforecast", async (WeatherForecast data, IAmazonSQS sqsClient, IOptions<SqsSettings> sqsOptions) =>
+{
+    var request = new SendMessageRequest()
+    {
+        MessageGroupId = data.City,
+        MessageDeduplicationId = data.Id.ToString(),
+        MessageBody = JsonSerializer.Serialize(data),
+        QueueUrl = sqsOptions.Value.QueueUrl
+    };
+
+    var result = await sqsClient.SendMessageAsync(request);
+})
+.WithName("PostWeatherForecast");
+
+app.Run();
+
+public class SqsSettings
+{
+    public string QueueUrl { get; set; } = string.Empty;
+}
+
+record WeatherForecast(Guid Id, string City,DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
+}
